e787bff9009dc46848c2803569d9814a3d6b092a
Erick Zanardo <erickzanardoo@gmail.com>
2024-08-13 13:36:44 -0300

    bootstrap

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flame Juice Box
+
+![Flame Juice Box](./flame_juice_box _small.png)
---
+++ lib/flame_juice_box.dart
@@ -0,0 +1,7 @@
+library flame_juice_box;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
